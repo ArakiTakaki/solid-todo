7d5ee9dd3122677caf448cedf8b099f271999ce4
ArakiTakaki <arakitakaki.work@gmail.com>
2022-02-16 21:37:49 +0900

    feat: 日付の扱いを定義

---
+++ src/interfaces/common.ts
@@ -0,0 +1,10 @@
+
+/**
+ * 日付の扱いに関して
+ * 
+ * ISO-8601形式
+ * ref https://ja.wikipedia.org/wiki/ISO_8601
+ * YYYY-MM-DDTHH:mm:ss.msZ
+ * ex: 2022-02-16T11:44:06.840Z
+ */
+export type ISODate = string;
